a6a2be85dc6f7c48a0ac42dc27c686539408fdcd
Israel Calderón de la Cruz <icalderond@outlook.com>
2021-01-18 09:05:05 -0600

    feat models: creating model layer

---
+++ Poke.Client.Models/PokeResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Poke.Client.Models
+{
+    public class PokeResult
+    {
+        [JsonProperty("count")]
+        public int Count { get; set; }
+
+        [JsonProperty("next")]
+        public string Next { get; set; }
+
+        [JsonProperty("previous")]
+        public object Previous { get; set; }
+
+        [JsonProperty("results")]
+        public List<Pokemon> Results { get; set; }
+    }
+}
---
+++ Poke.Client.Models/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Poke.Client.Models
+{
+    public class Pokemon
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}

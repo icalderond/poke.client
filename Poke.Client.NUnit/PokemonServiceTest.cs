e891dfd46ef0f244a1da20efe5bdc9557dde0b00
Israel Calderón de la Cruz <icalderond@outlook.com>
2021-01-18 09:06:41 -0600

    feat unit-test: creating unit test layer

---
+++ Poke.Client.NUnit/PokemonServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using Poke.Client.Services;
+
+namespace Poke.Client.NUnit
+{
+    [TestFixture()]
+    public class PokemonServiceTest
+    {
+        PokemonService PokemonService = new PokemonService();
+
+        [Test()]
+        public async Task Test_GetPokemonAsync_Get_IsSuccesfull()
+        {
+            var result = await PokemonService.GetPokemonAsync(20, 20);
+            Assert.IsNotNull(result);
+        }
+
+        [Test()]
+        public void Test_GetPokeImageAsync_Get_IsSuccesfull()
+        {
+            var result = PokemonService.GetPokeImage(1);
+            Assert.IsNotNull(result);
+        }
+    }
+}
